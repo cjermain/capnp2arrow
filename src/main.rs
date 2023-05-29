7f72bbcf9dd890353804885bcce1a591e1b3758d
Colin Jermain <cjermain@gmail.com>
2023-05-29 15:21:49 -0400

    Adding list support

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,15 @@ extern crate arrow2;
 use arrow2::datatypes::{DataType, Field, Schema};
 //use capnp::serialize_packed;
 use capnp::{dynamic_value, dynamic_struct};
+use capnp::schema::StructSchema;
 use capnp::introspect::TypeVariant;
 
 pub mod point_capnp {
     include!(concat!(env!("OUT_DIR"), "/point_capnp.rs"));
 }
 
-pub fn map_field(field: capnp::schema::Field) -> ::capnp::Result<Field> {
-    let name = field.get_proto().get_name().unwrap();
-    let nullable = true; // TODO: Determine nullable or remove comment
-    let capnp_dtype = field.get_type().which();
+
+pub fn map_dtype(capnp_dtype: TypeVariant) -> ::capnp::Result<DataType> {
     let arrow_dtype = match capnp_dtype {
         TypeVariant::Void => DataType::Null,
         TypeVariant::Bool => DataType::Boolean,
@@ -29,23 +28,34 @@ pub fn map_field(field: capnp::schema::Field) -> ::capnp::Result<Field> {
         TypeVariant::Float32 => DataType::Float32,
         TypeVariant::Float64 => DataType::Float64,
         TypeVariant::Text => DataType::Utf8, // always UTF8 and NUL-terminated
-        TypeVariant::Data => DataType::Null, // TODO: Fix
-        TypeVariant::Struct(st) => DataType::Null, // TODO: Fix
-        TypeVariant::List(l) => DataType::Null, // TODO: Fix
+        TypeVariant::Data => DataType::Binary, // TODO: Determine if this covers all cases
+        TypeVariant::Struct(st) => {
+            let fields = map_struct_fields(st.into()).unwrap();
+            DataType::Struct(fields)
+        }
+        TypeVariant::List(l) => {
+            let inner_dtype = map_dtype(l.which()).unwrap();
+            let inner_field = Field::new("", inner_dtype, true); // TODO: Determine nullable
+            DataType::List(Box::new(inner_field))
+        }
         TypeVariant::Enum(e) => DataType::Null, // TODO: Fix
         TypeVariant::AnyPointer => panic!("unsupported"),
         TypeVariant::Capability => panic!("unsupported")
     };
+    Ok(arrow_dtype)
+}
+
+pub fn map_field(field: capnp::schema::Field) -> ::capnp::Result<Field> {
+    let name = field.get_proto().get_name().unwrap();
+    let nullable = true; // TODO: Determine nullable or remove comment
+    let capnp_dtype = field.get_type().which();
+    let arrow_dtype = map_dtype(capnp_dtype).unwrap();
     Ok(Field::new(name, arrow_dtype, nullable))
 }
 
-//pub fn map_list_fields(name: String, reader: dynamic_list::Reader) -> ::capnp::Result<Field> {
-//}
-
-pub fn map_struct_fields(reader: dynamic_struct::Reader) -> ::capnp::Result<Vec<Field>> {
+pub fn map_struct_fields(schema: StructSchema) -> ::capnp::Result<Vec<Field>> {
     let mut fields = Vec::<Field>::new();
     let nullable = true; // TODO: Determine nullable
-    let schema = reader.get_schema();
     let non_union_fields = schema.get_non_union_fields()?;
     for field in non_union_fields {
         if field.get_type().is_pointer_type() {
@@ -64,7 +74,8 @@ pub fn map_struct_fields(reader: dynamic_struct::Reader) -> ::capnp::Result<Vec<
 
 
 pub fn map_schema(reader: dynamic_struct::Reader) -> ::capnp::Result<Schema> {
-    let fields = map_struct_fields(reader).unwrap();
+    let schema = reader.get_schema();
+    let fields = map_struct_fields(schema).unwrap();
     Ok(Schema::from(fields))
 }
 
