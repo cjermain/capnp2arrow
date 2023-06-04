e1997cddb1ae3915b57a59dd14c14e0f0f7c5f01
Colin Jermain <cjermain@gmail.com>
2023-06-04 09:30:48 -0400

    Adding Message struct to simplify

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ extern crate indexmap;
 extern crate arrow2;
 
 use capnp::{dynamic_value, serialize_packed};
+use capnp::message::Reader;
+use capnp::serialize::OwnedSegments;
 use capnp2arrow::schema::map_schema;
 use capnp2arrow::reader::allocate_array;
 use arrow2::chunk::Chunk;
@@ -18,27 +20,40 @@ pub mod point_capnp {
     include!(concat!(env!("OUT_DIR"), "/point_capnp.rs"));
 }
 
+
+struct Message {
+    reader: Reader<OwnedSegments>
+}
+
+impl Message {
+    pub fn new(buffer: &[u8]) -> Self {
+        Message {
+            reader: serialize_packed::read_message(
+                buffer,
+                ::capnp::message::ReaderOptions::new(),
+            ).unwrap()
+        }
+    }
+
+    pub fn to_dynamic(&self) -> dynamic_value::Reader {
+        self.reader.get_root::<point_capnp::point::Reader>().unwrap().into()
+    }
+}
+
+
+
 fn main() {
-    let stdin = ::std::io::stdin();
-    let mut stdin = stdin.lock();
-    let mut handle = stdin.fill_buf().unwrap();
-    let mut buffer = vec![];
+    let stdin = ::std::io::stdin().lock();
+
+    let readers: Vec<_> = stdin
+        .split(b'\n')
+        .map(|b| Message::new(b.unwrap().as_slice()))
+        .collect();
 
     // Get the schema from the first line
-    handle.read_until(b'\n', &mut buffer).unwrap();
-
-    let message_reader = serialize_packed::read_message(
-        buffer.as_slice(),
-        ::capnp::message::ReaderOptions::new(),
-    ).unwrap();
-    let reader = message_reader.get_root::<point_capnp::point::Reader>().unwrap();
-    let dynamic: dynamic_value::Reader = reader.into();
-
-    let schema = map_schema(dynamic.downcast()).unwrap();
+    let first_reader = readers[0].to_dynamic();
+    let schema = map_schema(first_reader.downcast()).unwrap();
     println!("{:?}", schema);
-
-    buffer.clear();
-
 
     let size = Some(0);
     let mut columns = schema
@@ -47,26 +62,7 @@ fn main() {
         .map(|f| (&f.name, allocate_array(f, size)))
         .collect::<HashMap<_, _>>();
 
-    // Loop over all messages
-    loop {
-        handle.read_until(b'\n', &mut buffer).unwrap();
-        if buffer.len() == 0 {
-            break
-        }
-
-        let message_reader = serialize_packed::read_message(
-            buffer.as_slice(),
-            ::capnp::message::ReaderOptions::new(),
-        ).unwrap();
-        let reader = message_reader.get_root::<point_capnp::point::Reader>().unwrap();
-        let dynamic: dynamic_value::Reader = reader.into();
-
-        println!("{:?}", dynamic);
-
-        // TODO: Fill in data
-
-        buffer.clear();
-    }
+    println!("{:?}", readers.iter().map(|r| r.to_dynamic()).collect::<Vec<_>>());
 
     let chunk = Chunk::new(
         columns.into_values().map(|mut ma| ma.as_box()).collect(),
