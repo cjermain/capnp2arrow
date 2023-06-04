c0ba72665a29f8da6df4aa6fb3d4aab3728f6154
Colin Jermain <cjermain@gmail.com>
2023-06-04 13:09:33 -0400

    Moving from pre-allocated columns to on-demand

--- src/reader.rs
+++ src/reader.rs
@@ -18,54 +18,57 @@ use capnp::Error; // TODO: Determine best Error
 use crate::schema::map_schema;
 
 
-pub fn allocate_array(field: &Field, size: Option<usize>) -> Box<dyn MutableArray> {
-    let size = size.unwrap_or(0);
+pub fn read_to_array<'a, A: Borrow<dynamic_value::Reader<'a>>>(
+    field: &Field,
+    values: &[A],
+) -> Box<dyn MutableArray> {
     match field.data_type() {
         DataType::Null => unimplemented!(), // TODO: NullArray,
-        DataType::Boolean => Box::new(MutableBooleanArray::with_capacity(size)),
-        DataType::Int8 => Box::new(MutablePrimitiveArray::<i8>::with_capacity(size)),
-        DataType::Int16 => Box::new(MutablePrimitiveArray::<i16>::with_capacity(size)),
-        DataType::Int32 => Box::new(MutablePrimitiveArray::<i32>::with_capacity(size)),
-        DataType::Int64 => Box::new(MutablePrimitiveArray::<i64>::with_capacity(size)),
-        DataType::UInt8 => Box::new(MutablePrimitiveArray::<u8>::with_capacity(size)),
-        DataType::UInt16 => Box::new(MutablePrimitiveArray::<u16>::with_capacity(size)),
-        DataType::UInt32 => Box::new(MutablePrimitiveArray::<u32>::with_capacity(size)),
-        DataType::UInt64 => Box::new(MutablePrimitiveArray::<u64>::with_capacity(size)),
-        DataType::Float32 => Box::new(MutablePrimitiveArray::<f32>::with_capacity(size)),
-        DataType::Float64 => Box::new(MutablePrimitiveArray::<f64>::with_capacity(size)),
-        DataType::Utf8 => Box::new(MutableUtf8Array::<i32>::with_capacity(size)),
-        DataType::Binary => Box::new(MutableBinaryArray::<i32>::with_capacity(size)),
-        DataType::Struct(fields) => {
-            let values = fields.iter().map(|f| allocate_array(f, Some(size))).collect();
-            Box::new(MutableStructArray::new(field.data_type().clone(), values))
+        DataType::Boolean =>  {
+            let iter = values.iter().map(|m| match m.borrow() {
+                dynamic_value::Reader::Bool(x) => Some(x),
+                _ => None
+            });
+            Box::new(MutableBooleanArray::from_trusted_len_iter(iter))
         }
-        DataType::List(inner) => {
-            let inner_array = allocate_array(inner, Some(size));
-            let inner_dtype = inner.data_type().clone();
-            Box::new(MutableListArray::<i32, _>::new_from(inner_array, inner_dtype, size))
-        }
-        //TypeVariant::Enum(_e) => DataType::Null, // TODO: Fix
-        //TypeVariant::AnyPointer => panic!("unsupported"),
-        //TypeVariant::Capability => panic!("unsupported"),
-        _ => todo!()
-
-    }
-}
-
-
-pub fn fill_array<'a, A: Borrow<dynamic_value::Reader<'a>>>(
-    column: &mut Box<dyn MutableArray>,
-    values: &[A],
-) {
-    match column.data_type() {
+        DataType::Int8 => todo!(), //Box::new(MutablePrimitiveArray::<i8>::with_capacity(size)),
+        DataType::Int16 => todo!(), //Box::new(MutablePrimitiveArray::<i16>::with_capacity(size)),
+        DataType::Int32 => todo!(), //Box::new(MutablePrimitiveArray::<i32>::with_capacity(size)),
+        DataType::Int64 => todo!(), //Box::new(MutablePrimitiveArray::<i64>::with_capacity(size)),
+        DataType::UInt8 => todo!(), //Box::new(MutablePrimitiveArray::<u8>::with_capacity(size)),
+        DataType::UInt16 => todo!(), //Box::new(MutablePrimitiveArray::<u16>::with_capacity(size)),
+        DataType::UInt32 => todo!(), //Box::new(MutablePrimitiveArray::<u32>::with_capacity(size)),
+        DataType::UInt64 => todo!(), //Box::new(MutablePrimitiveArray::<u64>::with_capacity(size)),
         DataType::Float32 => {
-            let column = column.as_mut_any().downcast_mut::<MutablePrimitiveArray<f32>>().unwrap();
             let iter = values.iter().map(|m| match m.borrow() {
                 dynamic_value::Reader::Float32(x) => Some(x),
                 _ => None
             });
-            column.extend_trusted_len(iter);
+            Box::new(MutablePrimitiveArray::<f32>::from_trusted_len_iter(iter))
         }
+        DataType::Float64 => {
+            let iter = values.iter().map(|m| match m.borrow() {
+                dynamic_value::Reader::Float64(x) => Some(x),
+                _ => None
+            });
+            Box::new(MutablePrimitiveArray::<f64>::from_trusted_len_iter(iter))
+        }
+        DataType::Utf8 => todo!(), //Box::new(MutableUtf8Array::<i32>::with_capacity(size)),
+        DataType::Binary => todo!(), //Box::new(MutableBinaryArray::<i32>::with_capacity(size)),
+        DataType::Struct(fields) => {
+            todo!();
+            //let values = fields.iter().map(|f| allocate_array(f, Some(size))).collect();
+            //Box::new(MutableStructArray::new(field.data_type().clone(), values))
+        }
+        DataType::List(inner) => {
+            todo!();
+            //let inner_array = allocate_array(inner, Some(size));
+            //let inner_dtype = inner.data_type().clone();
+            //Box::new(MutableListArray::<i32, _>::new_from(inner_array, inner_dtype, size))
+        }
+        //TypeVariant::Enum(_e) => DataType::Null, // TODO: Fix
+        //TypeVariant::AnyPointer => panic!("unsupported"),
+        //TypeVariant::Capability => panic!("unsupported"),
         _ => todo!()
     }
 }
@@ -81,23 +84,23 @@ pub fn read_to_chunk<'a, A: Borrow<dynamic_value::Reader<'a>>>(
     values: &[A],
     schema: &Schema,
 ) -> Result<Chunk<Box<dyn Array>>, Error>  {
-    let size = Some(0);
-    let mut columns = schema
+    let arrays = schema
         .fields
         .iter()
-        .map(|f| (&f.name, allocate_array(f, size)))
+        .map(|f| {
+            let vals: Vec<_> = values
+                .iter()
+                .map(|v| v.borrow()
+                     .downcast::<dynamic_struct::Reader>()
+                     .get_named(&f.name)
+                     .unwrap()
+                )
+                .collect();
+            (&f.name, read_to_array(f, vals.as_slice()))
+        })
         .collect::<HashMap<_, _>>();
 
-    schema.fields.iter().for_each(|f| {
-        let column = columns.get_mut(&f.name).unwrap();
-        let vals: Vec<_> = values
-            .iter()
-            .map(|v| v.borrow().downcast::<dynamic_struct::Reader>().get_named(&f.name).unwrap())
-            .collect();
-        fill_array(column, vals.as_slice());
-    });
-
     Ok(Chunk::new(
-        columns.into_values().map(|mut ma| ma.as_box()).collect(),
+        arrays.into_values().map(|mut ma| ma.as_box()).collect(),
     ))
 }
